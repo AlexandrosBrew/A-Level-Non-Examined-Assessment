1c41688a6af109129ab3c0b7f190a169ed4829cc
ValiLalioti <67018297+ValiLalioti@users.noreply.github.com>
2022-10-15 18:44:30 +0100

    final edits for cycle 1 screw this...

--- validation.py
+++ validation.py
@@ -1,22 +1,31 @@
 import sqlite3
+from createDatabase import load_database
 
-#!Compares the value that is going to be entered to the validation rules and returns True/False depending if it meets the requirements
-def validate_edit(attri, newVal, table):
-    with sqlite3.connect('SignTeach.db') as db:
-        cursor = db.cursor()
-        cursor.execute('SELECT * FROM '+table)
-        attributes = list(map(lambda x: x[0], cursor.description))
-        cursor.execute('SELECT * FROM '+table)
-        IDs = cursor.fetchall()
-        if attri in attributes and len(newVal) != 0:
+class Validation():
+    def __init__(self):
+        load_database()
+
+    def validate_edit(self, attri, newVal, table):
+        with sqlite3.connect('SignTeach.db') as db:
+            cursor = db.cursor()
+            cursor.execute('SELECT * FROM '+table)
+            attributes = list(map(lambda x: x[0], cursor.description))
+            if attri in attributes and len(newVal) != 0:
+                return True
+            else:
+                print('Failed Validation to edit')
+                return False
+
+    def validate_add(self, newData):
+        for i in range(len(newData)):
+            if len(newData[i]) == 0:
+                print('Failed Validation to add')
+                return False
+        return True
+
+    def validate_ID(self, ID, attriID):
+        if ID <= len(attriID):
             return True
         else:
-            print('Failed Validation')
+            print('Failed validation for ID')
             return False
-
-def validate_add(newData):
-    for i in range(len(newData)):
-        if len(newData[i]) == 0:
-            print('Failed Validation')
-            return False
-    return True
